3442637345b4e9e947cb01de63fbb3d54010eb7e
Francesco Giucastro <francesco.giucastro@link-me.it>
2018-09-08 09:50:09 +0200

    Added photo/image picker, added instagram and facebook share

--- App.js
+++ App.js
@@ -1,13 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import Share from 'react-native-share';
 
-export default class App extends React.Component {
+const optionsCamera = {
+  quality: 1.0,
+  maxWidth: 1920,
+  maxHeight: 1080,
+  storageOptions: {
+    cameraRoll: true,
+    skipBackup: false,
+    waitUntilSaved: true,
+  },
+};
+
+const optionsLibrary = {
+  quality: 1.0,
+  maxWidth: 1920,
+  maxHeight: 1080,
+  storageOptions: {
+    skipBackup: true,
+  },
+};
+
+export default class App extends Component {
+
+  openCamera = (social) => {
+    // Launch Camera:
+    ImagePicker.launchCamera(optionsCamera, (response) => {
+      if (response.didCancel || response.error) return;
+
+      const shareOptions = {
+        url: response.uri,
+        social: social === 'instagram' ? Share.Social.INSTAGRAM : Share.Social.FACEBOOK
+      };
+
+      Share.shareSingle(shareOptions);
+    });
+  }
+
+  openFileBrowser = (social) => {
+    // Open Image Library:
+    ImagePicker.launchImageLibrary(optionsLibrary, (response) => {
+      if (response.didCancel || response.error) return;
+
+      const shareOptions = {
+        url: response.uri,
+        social: social === 'instagram' ? Share.Social.INSTAGRAM : Share.Social.FACEBOOK
+      };
+
+      Share.shareSingle(shareOptions);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TouchableHighlight style={styles.button} onPress={() => this.openCamera('facebook')}>
+          <Text>Share Live Photo on Facebook</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button} onPress={() => this.openCamera('instagram')}>
+          <Text>Share Live Photo on Instagram</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button} onPress={() => this.openFileBrowser('facebook')}>
+          <Text>Share Local Image on Facebook</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button} onPress={() => this.openFileBrowser('instagram')}>
+          <Text>Share Local Image on Instagram</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -20,4 +80,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    justifyContent: 'center',
+    margin: 20
+  }
 });
--- android/app/src/main/java/com/rnshareexample/MainApplication.java
+++ android/app/src/main/java/com/rnshareexample/MainApplication.java
@@ -3,15 +3,18 @@ package com.rnshareexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import cl.json.RNSharePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import cl.json.ShareApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends Application implements ShareApplication, ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -22,7 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNSharePackage()
       );
     }
 
@@ -35,6 +40,11 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
+  }
+
+  @Override
+  public String getFileProviderAuthority() {
+    return "com.rnshareexample.provider";
   }
 
   @Override
